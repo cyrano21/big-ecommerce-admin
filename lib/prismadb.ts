20143cfe1429f9ea57a062d6f42bd9c03d2587ba
cyrano21 <louiscyrano@gmail.com>
2024-12-24 11:11:27 +0100

    reppair color create 3

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,10 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+};
+
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
+const prismadb = globalThis.prisma ?? prismaClientSingleton();
+
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
